c0b87d6aded9def30607fe414a306f5b1a206335
Pattyima <pattynaries@email.com>
2023-09-12 17:54:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - 1-alphabet.c
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char  ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
