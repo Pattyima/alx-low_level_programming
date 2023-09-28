e2c0894eee70640ab822970ead9fc79398cf02ee
Pattyima <pattynaries@email.com>
2023-09-28 16:33:11 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,13 +11,13 @@
  */
 char *test(char *s5, char *s6, char *t5, char *t6)
 {
-	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0');
-			return (t5);
-			else if (*s5 != *s6)
-			return (wildcard(s5, t6));
-			else
-			return (test(s5 + 1, s6 + 1, t5, t6));
-			}
+	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
+		return (t5);
+	else if (*s5 != *s6)
+		return (wildcard(s5, t6));
+	else
+		return (test(s5 + 1, s6 + 1, t5, t6));
+}
 /**
  * wildcard - checks for the wildcards
  * @s3: string to compare to
@@ -29,9 +29,9 @@ char *wildcard(char *s3, char *s4)
 {
 	if (*s3 != *s4)
 	{
-	if (*s3 == '\0')
-	return (0);
-	return (wildcard(s3 + 1, s4));
+		if (*s3 == '\0')
+			return (0);
+		return (wildcard(s3 + 1, s4));
 	}
 	return (test(s3 + 1, s4 + 1, s3, s4));
 }
@@ -46,7 +46,7 @@ char *wildcard(char *s3, char *s4)
 int wildcmp(char *s1, char *s2)
 {
 	char *p;
-	
+
 	if (*s2 == '*' && *(s2 + 1) == '\0')
 		return (1);
 	else if (*s2 == '*' && *(s2 + 1) == '*')
@@ -63,4 +63,5 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	return (wildcmp(s1 + 1, s2 + 1));
+}
 }
